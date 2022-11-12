3d01459b922cd79357bed30e6f14cbceb7f1d8ac
chau <chau1282001@gmail.com>
2022-11-12 21:34:27 +0700

    aaaa

---
+++ DOANCuoiKyNET/Areas/Admin/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DOANCuoiKyNET.Areas.Admin.Models
+{
+    public class Employee
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+    }
+}
---
+++ DOANCuoiKyNET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DOANCuoiKyNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
